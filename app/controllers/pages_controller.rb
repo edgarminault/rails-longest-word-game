b08a682a9b5379db9780a59c7be97d8a40af566e
Edgar Minault <e.minault@bluewin.ch>
2020-05-11 14:30:25 +0200

    Components inserted in page new. Montserrat font inserted.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,13 @@
 class PagesController < ApplicationController
 
   def new
+    generate_random_letters
+  end
+
+  private
+
+  def generate_random_letters
+    @array = (1..10).to_a.map { ('a'..'z').to_a[rand(26)] }
   end
 
 end
